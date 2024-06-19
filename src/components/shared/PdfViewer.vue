74e23eebd6029e110d770cb08f510c45a245815d
marenga14 <marengajulius@gmail.com>
2024-06-20 00:03:40 +0300

    ...fix: pdf viewing

--- src/components/shared/PdfViewer.vue
+++ src/components/shared/PdfViewer.vue
@@ -1,93 +1,100 @@
 <template>
-  <div >
+  <div>
     <h2 v-if="title" class="font-bold dashed-border">
       {{ title }}
     </h2>
 
     <div class="flex flex-column justify-center items-center">
       <div class="flex flex-row justify-center">
-
-        <button class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md
-              hover:bg-primary2"
-                :class="changePageCheck('prev')" v-on:click="prevPage"
+        <button
+          class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md hover:bg-primary2"
+          :class="changePageCheck('prev')"
+          v-on:click="prevPage"
         >
           Prev
         </button>
         <span class="px-3 py-1">
-          <span class="font-bold">{{ currentPage }}</span>/<span class="text-sm">{{ numPages }}</span>
+          <span class="font-bold">{{ currentPage }}</span
+          >/<span class="text-sm">{{ numPages }}</span>
         </span>
-        <button class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md
-              hover:bg-primary2" v-on:click="nextPage"
-                :class="changePageCheck('next')">
+        <button
+          class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md hover:bg-primary2"
+          v-on:click="nextPage"
+          :class="changePageCheck('next')"
+        >
           Next
         </button>
       </div>
-      <pdf :src="pdfSrc" :page="currentPage" class="border-1 border-gradient shadow-sm"
-            style="margin:10px auto;"
-           :scale="scale">
+      <pdf
+        :src="pdfSrc"
+        :page="currentPage"
+        class="border-1 border-gray-200 px-2 container border-gradient shadow-sm"
+        style="margin: 10px auto"
+        :scale="scale"
+      >
       </pdf>
-<!--      <div class="flex flex-row justify-center">-->
-<!--        <button class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md-->
-<!--              hover:bg-primary2"-->
-<!--                :class="changePageCheck('prev')" v-on:click="prevPage">-->
-<!--          Prev-->
-<!--        </button>-->
-<!--        <span class="px-3 py-1">-->
-<!--          <span class="font-bold">{{ currentPage }}</span>/<span class="text-sm">{{ numPages }}</span>-->
-<!--        </span>-->
-<!--        <button class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md-->
-<!--              hover:bg-primary2" v-on:click="nextPage"-->
-<!--                :class="changePageCheck('prev')" >-->
-<!--          Next-->
-<!--        </button>-->
-<!--      </div>-->
+      <!--      <div class="flex flex-row justify-center">-->
+      <!--        <button class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md-->
+      <!--              hover:bg-primary2"-->
+      <!--                :class="changePageCheck('prev')" v-on:click="prevPage">-->
+      <!--          Prev-->
+      <!--        </button>-->
+      <!--        <span class="px-3 py-1">-->
+      <!--          <span class="font-bold">{{ currentPage }}</span>/<span class="text-sm">{{ numPages }}</span>-->
+      <!--        </span>-->
+      <!--        <button class="px-3 py-1 bg-primary2 text-white mx-2 rounded-md-->
+      <!--              hover:bg-primary2" v-on:click="nextPage"-->
+      <!--                :class="changePageCheck('prev')" >-->
+      <!--          Next-->
+      <!--        </button>-->
+      <!--      </div>-->
     </div>
   </div>
 </template>
 
 <script>
-import pdf from 'pdfvuer'
-import {createLoadingTask} from 'pdfvuer'
+import pdf from "pdfvuer";
+import { createLoadingTask } from "pdfvuer";
 
 export default {
   name: "PdfViewer",
   components: {
-    pdf
+    pdf,
   },
   props: {
     pdfSrc: String,
-    title: String
+    title: String,
   },
-  data () {
+  data() {
     return {
       currentPage: 1,
       numPages: 0,
       pdfdata: undefined,
       errors: [],
-      scale: 'page-width'
-    }
+      scale: "page-width",
+    };
   },
   methods: {
     nextPage() {
-      if (this.currentPage < this.numPages) this.currentPage += 1
+      if (this.currentPage < this.numPages) this.currentPage += 1;
     },
     prevPage() {
-      if (this.currentPage !== 1)this.currentPage -= 1
+      if (this.currentPage !== 1) this.currentPage -= 1;
     },
     changePageCheck(direction) {
-      if (direction === 'prev' && this.currentPage <= 1) return 'hover:cursor-not-allowed'
-      else if (direction === 'next' && this.currentPage === this.numPages) return 'hover:cursor-not-allowed'
-    }
+      if (direction === "prev" && this.currentPage <= 1)
+        return "hover:cursor-not-allowed";
+      else if (direction === "next" && this.currentPage === this.numPages)
+        return "hover:cursor-not-allowed";
+    },
   },
   mounted() {
-    this.pdfdata = createLoadingTask(this.pdfSrc)
-    this.pdfdata.then(pdf => {
-      this.numPages = pdf.numPages
-    })
-  }
-}
+    this.pdfdata = createLoadingTask(this.pdfSrc);
+    this.pdfdata.then((pdf) => {
+      this.numPages = pdf.numPages;
+    });
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
