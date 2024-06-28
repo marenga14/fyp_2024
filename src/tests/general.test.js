fdfa4f2ddd21978ece98e8fccce964a3d14b2299
marenga14 <marengajulius@gmail.com>
2024-06-28 23:25:26 +0300

    ..

--- src/tests/general.test.js
+++ src/tests/general.test.js
@@ -1,9 +1,10 @@
-//example of the test
+// //example of the test
 
 import { expect, test, it } from "vitest";
+import "@testing-library/vue";
+import App from "../views/dashboard.vue";
+import { render, screen } from "@testing-library/vue";
 
 test("Prove the vitest works with vue", async () => {
   expect(2 + 3).toBe(5);
 });
-
-// implementing the c
