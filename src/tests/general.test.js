22b1a6e33b4949ae74379e2ded9c034cb90392da
marenga14 <marengajulius@gmail.com>
2024-07-01 23:12:07 +0300

    ..

--- src/tests/general.test.js
+++ src/tests/general.test.js
@@ -1,10 +1,8 @@
-// //example of the test
+// import { expect, test, it } from "vitest";
+// import "@testing-library/vue";
+// import App from "../views/dashboard.vue";
+// import { render, screen } from "@testing-library/vue";
 
-import { expect, test, it } from "vitest";
-import "@testing-library/vue";
-import App from "../views/dashboard.vue";
-import { render, screen } from "@testing-library/vue";
-
-test("Prove the vitest works with vue", async () => {
-  expect(2 + 3).toBe(5);
-});
+// test("Prove the vitest works with vue", async () => {
+//   expect(2 + 3).toBe(5);
+// });
